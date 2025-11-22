414fd470738f87be9117bea4f43d4394adb58474
Juan David Guerrero Uchima <dj.jd2009@hotmail.com>
2025-11-22 14:11:51 -0500

    Animation For My game

--- Assets/Scripts/Ligths/MirrorsManager.cs
+++ Assets/Scripts/Ligths/MirrorsManager.cs
@@ -1,20 +1,57 @@
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
 
-public class MirrorsManager : MonoBehaviour
+public class MirrorManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public List<MirrorMoveController> mirrors = new List<MirrorMoveController>();
+    [Tooltip("Lista de objetos que tienen LigthsController (espejos)")]
+    public List<GameObject> mirrors = new List<GameObject>();
 
+    private GameObject activeMirror;
 
-    void Start()
+    // void Start()
+    // {
+    //     // Opcional: si hay alguno marcado en la lista, activamos el primero
+    //     if (mirrors.Count > 0)
+    //         SetActiveMirror(mirrors[0]);
+    // }
+
+    public void SetActiveMirror(GameObject mirror)
     {
-        
+        if (mirror == activeMirror) return;
+
+        // Desactivar todos
+        for (int i = 0; i < mirrors.Count; i++)
+        {
+            var go = mirrors[i];
+            if (go == null) continue;
+            var lc = go.GetComponent<LigthsController>();
+            if (lc != null)
+            {
+                lc.ForceDeactivate(); // método seguro para desactivar
+            }
+        }
+
+        // Activar el seleccionado (si tiene LigthsController)
+        if (mirror != null)
+        {
+            var lc = mirror.GetComponent<LigthsController>();
+            if (lc != null)
+            {
+                lc.ForceActivate();
+                activeMirror = mirror;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Helper para registrar dinámicamente espejos (opcional)
+    public void RegisterMirror(GameObject mirror)
     {
-        
+        if (!mirrors.Contains(mirror)) mirrors.Add(mirror);
+    }
+
+    public void UnregisterMirror(GameObject mirror)
+    {
+        if (mirrors.Contains(mirror)) mirrors.Remove(mirror);
+        if (activeMirror == mirror) activeMirror = null;
     }
 }
---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public CameraToggle cameraToggle;
+    public GameObject EmiterLight;
+    public CanvasGroup menuCanvasGroup;
+    private bool canvasActive = true;
+    private bool lightOn = false;
+    private LigthsController ligthsController;
+
+    public MirrorManager mirrorManager;
+    void Start()
+    {
+        if (EmiterLight != null)
+        {
+            ligthsController = EmiterLight.GetComponent<LigthsController>();
+        }
+        if (ligthsController != null)
+        {
+            ligthsController.isFireLightOn= lightOn;
+        }
+    }
+
+
+    public void StartGame()
+    {
+        if (cameraToggle != null)
+        {
+            cameraToggle.SetCameraPriority();
+        }
+        // if (ligthsController != null)
+        // {
+        //     ligthsController.isFireLightOn= true;
+        // }
+        // if (EmiterLight != null)
+        // {
+        //     SpriteRenderer sr = EmiterLight.GetComponent<SpriteRenderer>();
+        //     if (sr != null)
+        //     {
+        //         sr.enabled = true;
+        //     }
+        // }
+        mirrorManager.SetActiveMirror(EmiterLight);
+        toggleCanvas();
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    } 
+
+    void toggleCanvas()
+    {
+        canvasActive = !canvasActive;
+        if (canvasActive)
+        {
+            menuCanvasGroup.alpha = 1;
+            menuCanvasGroup.interactable = true;
+            menuCanvasGroup.blocksRaycasts = true;
+        }
+        else
+        {
+            menuCanvasGroup.alpha = 0;
+            menuCanvasGroup.interactable = false;
+            menuCanvasGroup.blocksRaycasts = false;
+        }
+    }  
+    #if UNITY_EDITOR
+    void OnApplicationQuit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+    #endif
+}
